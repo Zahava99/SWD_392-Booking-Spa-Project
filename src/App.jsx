5dedbc864aa1d6e6f8c47f43d4ac2dcbe2211bc5
Hqmyl <142112839+hhq160325@users.noreply.github.com>
2025-02-05 21:00:40 +0700

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { router } from "./routes/routes";
+import { RouterProvider } from "react-router-dom";
+import "@fontsource/jost";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <RouterProvider router={router} />
+    </>
+  );
+}
+
+export default App;
