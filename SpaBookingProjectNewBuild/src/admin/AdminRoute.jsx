0bfadea34806870379d1e9a0d661dbdab846c25f
Hqmyl <142112839+hhq160325@users.noreply.github.com>
2025-03-27 00:35:02 +0700

    update admin
    remove page

--- SpaBookingProjectNewBuild/src/admin/AdminRoute.jsx
+++
@@ -1,6 +0,0 @@
-import { Route } from "react-router-dom";
-import AdminDashboard from "./AdminDashboard";
-
-export default function AdminRoute() {
-  return <Route path="/admin" element={<AdminDashboard />} />;
-}
