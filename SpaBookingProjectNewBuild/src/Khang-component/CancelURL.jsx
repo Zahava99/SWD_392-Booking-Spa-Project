4cf8f04dfbd5eb0c9c2a23e5c55d6a74f2c6aa51
Zahava99 <97643126+Zahava99@users.noreply.github.com>
2025-03-26 11:16:54 +0700

    Update CancelURL.jsx

--- SpaBookingProjectNewBuild/src/Khang-component/CancelURL.jsx
+++ SpaBookingProjectNewBuild/src/Khang-component/CancelURL.jsx
@@ -24,7 +24,7 @@ const CancelPayment = () => {
         <Button
           variant="contained"
           color="primary"
-          onClick={() => navigate("/payment")}
+          onClick={() => navigate("/")}
           sx={{ marginTop: "20px" }}
         >
           Quay lại thanh toán
